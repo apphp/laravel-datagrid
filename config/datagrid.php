6085f48c13c4e9f4da0ef0b2b09a116c4081cff0
Samuel Akopyan <leumas.a@gmail.com>
2021-01-02 15:38:42 +0200

    Added config file

---
+++ config/datagrid.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Vue Multiselect
+    |--------------------------------------------------------------------------
+    |
+    | Specifies whether to use multiselect or not
+    */
+    'vueMultiselect' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default messages
+    |--------------------------------------------------------------------------
+    |
+    | Default messages
+    */
+    'messages' => [
+        // Default warning message
+        'warning' => 'Sorry, no records were found. Please adjust your search criteria and try again.',
+    ],
+
+];
