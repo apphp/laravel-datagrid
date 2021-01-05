d9d3fc426250e02e7f14585e636cb72d3449dc47
Samuel Akopyan <leumas.a@gmail.com>
2021-01-05 13:24:44 +0200

    Implemented GridView functionality

--- src/Apphp/DataGrid/GridView.php
+++ src/Apphp/DataGrid/GridView.php
@@ -40,4 +40,63 @@ class GridView
         return self::$instance;
     }
 
-}
+    public static function renderTable($columns = null)
+    {
+        $output = '';
+
+        if ( !count(self::$records)) {
+            return $output;
+        }
+
+        if ( !is_array($columns) && !is_string($columns) ) {
+            return $output;
+        }
+
+        // Prepare columns, if no columns specified
+        if (empty($columns)) {
+            if ($record = self::$records->first()) {
+                $columns = array_merge([$record->getKeyName()], array_keys($record->attributesToArray()));
+                $columns = array_combine($columns, $columns);
+            }
+        }
+        ///dd($columns);
+
+        $output .= '<div class="table-responsive">';
+        $output .= '<table class="table table-bordered table-striped">';
+
+        // Render column names
+        $output .= '<thead>';
+        $output .= '<tr>';
+        foreach ($columns as $key => $column) {
+            $title = is_array($column) ? ($column['title'] ?? '') : $column;
+            $width = is_array($column) ? ($column['width'] ? ' width="'.$column['width'].'"' : '') : '';
+            $class = is_array($column) ? ($column['headClass'] ? ' class="'.$column['headClass'].'"' : '') : '';
+
+            $output .= '<th class="text-center"'.$width.$class.'>'.$title.'</th>';
+        }
+        $output .= '</tr>';
+        $output .= '</thead>';
+
+        // Render rows
+        $output .= '<tbody>';
+        foreach (self::$records as $record) {
+            $output .= '<tr>';
+            foreach ($columns as $key => $column) {
+                $columnKey = is_array($column) ? $key : $column;
+                $output .= '<td>';
+                $output .= $record[$columnKey] ?? '';
+                $output .= '</td>';
+            }
+            $output .= '</tr>';
+        }
+
+        $output .= '</tbody>';
+        $output .= '</table>';
+        $output .= '</div>';
+
+        return $output;
+    }
+
+
+};
+
