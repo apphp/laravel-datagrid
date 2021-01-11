5f059cf76fd742eaebf6782115f8cf00c91e8a75
Samuel Akopyan <leumas.a@gmail.com>
2021-01-11 12:13:12 +0200

    Created TestDataGridFilter test

---
+++ tests/TestDataGridFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Apphp\DataGrid\Tests;
+
+use Tests\TestCase;
+use Apphp\DataGrid\Filter;
+
+
+class TestDataGridFilter extends TestCase
+{
+
+    /**
+     * Test filter is empty
+     */
+    public function testA(): void
+    {
+        $this->assertEquals(1, 1);
+    }
+
+
+}
